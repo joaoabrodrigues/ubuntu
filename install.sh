2d4dd2b358a68593257d9533bf3627d4bdda50ac
joaoabrodrigues <jorodrigues@uolinc.com>
2020-05-11 15:54:09 -0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Removendo travas eventuais do apt ##
+
+sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
+
+## Atualizando o repositório ##
+
+sudo apt update && 
